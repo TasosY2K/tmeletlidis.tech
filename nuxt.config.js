4bf2a14e05bb23742355bdffeabcef743a4b2a15
TasosY2K <gustavolama20@gmail.com>
2021-06-16 02:33:02 +0300

    removed unused axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,12 +38,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
